de122119230257f89d73a2a23a710fb476bcde3f
David M <dg.mendez67@uniandes.edu.co>
2012-08-05 18:50:36 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ECloudsV01::Application.config.secret_token = '8004f02b75c30a69950210043ef0be6e49ad8a63945905021a81af98fb098c29ac31fd07936364be4741788b654dcda862b69986d100322a148a6434bab41685'
