81f58e53d67256a4a939ed59ba0198cd520d53c5
David M <dg.mendez67@uniandes.edu.co>
2012-11-13 17:25:23 -0500

    depsliegue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,4 +65,16 @@ ECloudsV01::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.default_url_options = { :host => '0.0.0.0:3000' }
+
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :user_name => 'nclopezo@gmail.com',
+      :password => 'E5G5yq6y',
+      :authentication => 'plain',
+      :enable_starttls_auto => true
+  }
+
 end
