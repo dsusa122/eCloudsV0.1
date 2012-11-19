86a739e151fb797911708775facad3a63cd6aea3
David M <dg.mendez67@uniandes.edu.co>
2012-11-19 09:01:55 -0500

    despliegueeee

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,25 +5,25 @@ ECloudsV01::Application.configure do
     #class ApplicationController; end
   #end
   
-  config.assets.initialize_on_precompile = false
+  #->config.assets.initialize_on_precompile = false
   # Code is not reloaded between requests
-  config.cache_classes = true
+  #->config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  #->config.consider_all_requests_local       = false
+  #->config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  #->config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  #->config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  #->config.assets.compile = false
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  #->config.assets.digest = true
 
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
@@ -61,16 +61,71 @@ ECloudsV01::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
-  config.i18n.fallbacks = true
+  #->config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation = :notify
+  #->config.active_support.deprecation = :notify
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'floating-wave-3280.herokuapp.com' }
+  #->config.action_mailer.default_url_options = { :host => 'floating-wave-3280.herokuapp.com' }
+
+ #-> config.action_mailer.smtp_settings = {
+     #-> :address => "smtp.gmail.com",
+     #-> :port => 587,
+     #-> :user_name => 'nclopezo@gmail.com',
+     #-> :password => 'E5G5yq6y',
+     #-> :authentication => 'plain',
+     #-> :enable_starttls_auto => true
+ #-> }
+
+ ####################################################
+
+ #################prueba desesperada!!!!!
+
+ ####################################################
+
+  Settings specified here will take precedence over those in config/application.rb
+
+  # In the development environment your application's code is reloaded on
+  # every request. This slows down response time but is perfect for development
+  # since you don't have to restart the web server when you make code changes.
+  config.cache_classes = false
+  
+
+  # Log error messages when you accidentally call methods on nil.
+  config.whiny_nils = true
+
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
+
+  # Only use best-standards-support built into browsers
+  config.action_dispatch.best_standards_support = :builtin
+
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
+
+  # Log the query plan for queries taking more than this (works
+  # with SQLite, MySQL, and PostgreSQL)
+  config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Do not compress assets
+  config.assets.compress = false
+
+  # Expands the lines which load the assets
+  config.assets.debug = true
+
+
+  config.action_mailer.default_url_options = { :host => '0.0.0.0:3000' }
 
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
@@ -80,5 +135,6 @@ ECloudsV01::Application.configure do
       :authentication => 'plain',
       :enable_starttls_auto => true
   }
+
 
 end
