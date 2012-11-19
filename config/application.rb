f640aebfcb7ca0906923546dc077f66f26dc2c21
David M <dg.mendez67@uniandes.edu.co>
2012-11-19 08:07:32 -0500

    despliegueeee

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-class ApplicationController < ActionController::Base
-
-end
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 ECloudsV01::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  module Devise
+    class ApplicationController; end
+  end
+  
   config.assets.initialize_on_precompile = false
   # Code is not reloaded between requests
   config.cache_classes = true
